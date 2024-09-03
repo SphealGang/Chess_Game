f0a68dfd07dc57b5dd19088d9d32e0748829381f
SphealGang <148595149+SphealGang@users.noreply.github.com>
2024-09-03 18:54:56 +0300

    piece_functionality.py

--- piece_functionality.py
+++ piece_functionality.py
@@ -7,25 +7,45 @@ def update_piece(rect,vector):
     rect.x = vector.x * square_size + 20
     rect.y = vector.y * square_size + 20
 
-def available_moves(current_position,piece_dict):
+def available_moves(piece_name,current_position,piece_dict):
     legal_squares = []
  
-    if current_position.y == 7:
-        legal_squares.append(current_position - pygame.Vector2(0,1))
-        legal_squares.append(current_position - pygame.Vector2(0,2))      
-    else:
-        legal_squares.append(current_position - pygame.Vector2(0,1))
+    if 'w_pawn' in piece_name:
+        if current_position.y == 7:
+            legal_squares.append(current_position - pygame.Vector2(0,1))
+            legal_squares.append(current_position - pygame.Vector2(0,2))      
+        else:
+            legal_squares.append(current_position - pygame.Vector2(0,1))
 
-    for y in legal_squares:
-        for piece in piece_dict:
-            if y == piece['position']:
-                legal_squares = []
+        for y in legal_squares:
+            for piece in piece_dict:
+                if y == piece['position']:
+                    legal_squares = []
 
-    for square in piece_dict:
-        if (current_position - pygame.Vector2(0,1) + pygame.Vector2(1,0)) == square['position']and square['name'][0] == 'b':
-            legal_squares.append(current_position - pygame.Vector2(0,1) + pygame.Vector2(1,0))
-        elif (current_position - pygame.Vector2(0,1) - pygame.Vector2(1,0)) == square['position'] and square['name'][0] == 'b':
-            legal_squares.append(current_position - pygame.Vector2(0,1) - pygame.Vector2(1,0))
+        for square in piece_dict:
+            if (current_position - pygame.Vector2(0,1) + pygame.Vector2(1,0)) == square['position']and square['name'][0] == 'b':
+                legal_squares.append(current_position - pygame.Vector2(0,1) + pygame.Vector2(1,0))
+            elif (current_position - pygame.Vector2(0,1) - pygame.Vector2(1,0)) == square['position'] and square['name'][0] == 'b':
+                legal_squares.append(current_position - pygame.Vector2(0,1) - pygame.Vector2(1,0))
+        
+    elif 'b_pawn' in piece_name:
+        if current_position.y == 2:
+            legal_squares.append(current_position + pygame.Vector2(0,1))
+            legal_squares.append(current_position + pygame.Vector2(0,2))      
+        else:
+            legal_squares.append(current_position + pygame.Vector2(0,1))
+
+        for y in legal_squares:
+            for piece in piece_dict:
+                if y == piece['position']:
+                    legal_squares = []
+
+        for square in piece_dict:
+            if (current_position + pygame.Vector2(0,1) + pygame.Vector2(1,0)) == square['position']and square['name'][0] == 'w':
+                legal_squares.append(current_position + pygame.Vector2(0,1) + pygame.Vector2(1,0))
+            elif (current_position + pygame.Vector2(0,1) - pygame.Vector2(1,0)) == square['position'] and square['name'][0] == 'w':
+                legal_squares.append(current_position + pygame.Vector2(0,1) - pygame.Vector2(1,0))
+
 
     return legal_squares
 
@@ -43,18 +63,3 @@ def display_possible_moves(x,screen):
         color = 'orange' if (square.x / 100 + square.y / 100) % 2 == 0 else "red"
         pygame.draw.rect(screen, color, square)    
     
-# def pawn_movement(x):
-#     print("Before move:")
-#     print(f"Possible moves: {x['possible_moves']}")
-#     print(f"Position: {x['position']}")
-#     print('-----------------')
-
-#     x['position'] = x['position'] + pygame.Vector2(0, -1)
-#     update_piece(x['rect'],x['position'])
-#     x['position'] = x['position'] = pygame.math.Vector2(x['position'])
-#     x['possible_moves'] = available_moves_white_pawn(x['position'])
-
-#     print("After move:")
-#     print(f"Possible moves: {x['possible_moves']}")
-#     print(f"Position: {x['position']}")
-#     print('-----------------')
