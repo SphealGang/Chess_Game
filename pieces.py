d518be94c2d313ef0096f7992d4cbaf77a69d290
SphealGang <148595149+SphealGang@users.noreply.github.com>
2024-09-13 15:53:37 +0300

    Update pieces.py

--- pieces.py
+++ pieces.py
@@ -1,50 +1,75 @@
 import pygame
 from piece_functionality import *
 
+# pieces = [
+#     {'name':'w_pawn1',
+#      'position':pygame.Vector2(1, 7),
+#      'color':'yellow',
+#       'possible_moves':[]
+#      },
+
+#      {'name':'w_pawn2',
+#       'position': pygame.Vector2(7, 4),
+#       'color':'yellow',
+#       'possible_moves':[]
+#       },
+
+#       {'name':'w_pawn3',
+#       'position': pygame.Vector2(1, 4),
+#       'color':'yellow',
+#       'possible_moves':[]
+#       },
+#       {'name':'w_rook2',
+#       'position': pygame.Vector2(8, 8),
+#       'color':'yellow',
+#       'possible_moves':[]
+#       },
+#       {'name':'b_pawn1',
+#      'position':pygame.Vector2(1, 2),
+#      'color':'red',
+#       'possible_moves':[]
+#      },
+
+#      {'name':'b_pawn2',
+#       'position': pygame.Vector2(2, 3),
+#       'color':'red',
+#       'possible_moves':[]
+#       },
+
+#       {'name':'b_pawn3',
+#       'position': pygame.Vector2(6, 2),
+#       'color':'red',
+#       'possible_moves':[]
+#       },
+
+#       {'name':'w_bishop1',
+#       'position': pygame.Vector2(3, 8),
+#       'color':'yellow',
+#       'possible_moves':[]
+#       },
+
+#       {'name':'w_queen',
+#       'position': pygame.Vector2(5, 6),
+#       'color':'yellow',
+#       'possible_moves':[]
+#       },
+
+#       {'name':'w_horse1',
+#      'position':pygame.Vector2(4, 4),
+#      'color':'yellow',
+#       'possible_moves':[]
+#      }
+#     ]
+
 pieces = [
-    {'name':'w_pawn1',
-     'position':pygame.Vector2(1, 7),
+     {'name':'b_pawn7',
+     'position':pygame.Vector2(5, 2),
      'color':'yellow',
       'possible_moves':[]
      },
-
-     {'name':'w_pawn2',
-      'position': pygame.Vector2(7, 4),
-      'color':'yellow',
+    {'name':'w_king',
+     'position':pygame.Vector2(4, 4),
+     'color':'yellow',
       'possible_moves':[]
-      },
-
-      {'name':'w_pawn3',
-      'position': pygame.Vector2(1, 4),
-      'color':'yellow',
-      'possible_moves':[]
-      },
-      {'name':'w_rook2',
-      'position': pygame.Vector2(8, 8),
-      'color':'yellow',
-      'possible_moves':[]
-      },
-      {'name':'b_pawn1',
-     'position':pygame.Vector2(1, 2),
-     'color':'red',
-      'possible_moves':[]
-     },
-
-     {'name':'b_pawn2',
-      'position': pygame.Vector2(2, 3),
-      'color':'red',
-      'possible_moves':[]
-      },
-
-      {'name':'b_pawn3',
-      'position': pygame.Vector2(6, 2),
-      'color':'red',
-      'possible_moves':[]
-      },
-
-      {'name':'w_bishop',
-      'position': pygame.Vector2(3, 8),
-      'color':'yellow',
-      'possible_moves':[]
-      }
-    ]
+     }
+     ]
