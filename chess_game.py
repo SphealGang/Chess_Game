19c1d7baabbcdbfba8adcf2cb8cbfbd182cb9c6b
SphealGang <148595149+SphealGang@users.noreply.github.com>
2024-12-03 22:13:33 +0200

    Update chess_game.py

--- chess_game.py
+++ chess_game.py
@@ -29,7 +29,6 @@ class PieceManager():
         for x in self.pieces:
             piece_pos = x['position']
             piece_rect = pygame.Rect(piece_pos.x * square_size + padding, piece_pos.y * square_size + padding, 60, 60)
-
             x.update({'rect': piece_rect})
 
     def create_possible_moves(self):
@@ -86,6 +85,8 @@ def select_piece():
             for y in selected_piece['possible_move_square']:
                 if y.collidepoint(mouse_pos):
                     if pygame.mouse.get_pressed()[0] == 1:
+                        is_check(turn_manager,pieces_init.pieces)
+
                         team = 'w' if turn_manager == 3 else 'b'
                         king = next((x for x in pieces_init.pieces if team + '_king' in x['name']))
 
@@ -95,19 +96,15 @@ def select_piece():
                                 castle_squares = [pygame.Vector2(3,y.y/100),pygame.Vector2(7,y.y/100)]
                                 if selected_square in castle_squares:
                                     if selected_square == castle_squares[0]:
-                                        castle_movement(selected_piece,rook = next((x for x in pieces_init.pieces if 'rook1' in x['name'] and selected_piece['name'][0] == x['name'][0]), None),new_king_position=castle_squares[0],new_rook_position=pygame.Vector2(1,0),piece_dict=pieces_init.pieces)
-                                    elif selected_square == castle_squares[1]:
-                                        castle_movement(selected_piece,rook = next((x for x in pieces_init.pieces if 'rook2' in x['name'] and selected_piece['name'][0] == x['name'][0]), None),new_king_position=castle_squares[1],new_rook_position=pygame.Vector2(-1,0),piece_dict=pieces_init.pieces)
+                                        rook1= next((x for x in pieces_init.pieces if 'rook1' in x['name'] and selected_piece['name'][0] == x['name'][0]), None)
+                                        rook1_end_pos = pygame.Rect(y.x + 100,y.y,100,100)
 
-                        if king['check'] == True:
-                            if selected_piece['name'] != king['name']:
-                                main_game()
-                                if turn_manager == 4:
-                                    turn_manager = 2
-                                else:
-                                    turn_manager = 1
-                                break
-                        pass
+                                        castle_movement(selected_piece,rook = rook1 ,new_king_position=castle_squares[0],new_rook_position=pygame.Vector2(1,0),piece_dict=pieces_init.pieces,func=smooth_movement(rook1['rect'],rook1_end_pos,rook1))
+                                    elif selected_square == castle_squares[1]:
+                                        rook2= next((x for x in pieces_init.pieces if 'rook2' in x['name'] and selected_piece['name'][0] == x['name'][0]), None)
+                                        rook2_end_pos = pygame.Rect(y.x - 100,y.y,100,100)
+                                        
+                                        castle_movement(selected_piece,rook = rook2,new_king_position=castle_squares[1],new_rook_position=pygame.Vector2(-1,0),piece_dict=pieces_init.pieces,func=smooth_movement(rook2['rect'],rook2_end_pos,rook2))
 
                         previous_position = selected_piece['position']
                         new_position = pygame.Vector2(y.x/100,y.y/100)
@@ -115,7 +112,6 @@ def select_piece():
                         selected_piece.update({'position':new_position})
 
                         taken_piece = [x for x in pieces_init.pieces if selected_piece['position'] == x['position'] and x['name'] != selected_piece]
-
                         taken_piece_copy = taken_piece
 
                         missed_en_passant(selected_piece,pieces_init.pieces)
@@ -130,28 +126,13 @@ def select_piece():
                             else:
                                 pass
 
-                        if 'king' not in selected_piece['name']:
-                            if self_check(pieces_init.pieces,selected_piece,taken_piece) == True:
-                                selected_piece.update({'position':previous_position})
-                                pieces_init.pieces += taken_piece_copy
-                                main_game()
-                                if turn_manager == 4:
-                                    turn_manager = 2
-                                else:
-                                    turn_manager = 1
-                                break
-                            else:
-                                pass  
-
                         delete_ep_pawn(selected_piece,pieces_init.pieces)
 
                         change_first_move(selected_piece)
 
                         change_first_move_2_squares(selected_piece,previous_position,new_position)
 
-                        pieces_init.create_possible_moves()
-
-                        reset_check(selected_piece)
+                        is_check(turn_manager,pieces_init.pieces)
 
                         if king['check'] == True:
                             selected_piece.update({'position':previous_position})
@@ -163,28 +144,74 @@ def select_piece():
                                 turn_manager = 1
                             break
                         
-
-                        # if 'check' in selected_piece.keys():
-                        #     print("#########################")
-                        #     print(selected_piece['name'])
-                        #     print(selected_piece['check'])
-
-                        is_check(turn_manager,pieces_init.pieces)
-
                         if turn_manager == 3:
                             turn_manager = 2
                         else:
                             turn_manager = 1
 
+                        smooth_movement(selected_piece['rect'],y,selected_piece)
+
                         main_game()
+
+                        if 'pawn' in selected_piece['name']:
+                            opposite_side = 1 if team == 'w' else 8
+                            if selected_piece['position'].y == opposite_side:
+                                turn_manager = 5 if opposite_side == 1 else 6
                         
                         break
 
+    elif turn_manager == 5 or turn_manager == 6:
+        buttons = [{'rect': pygame.Rect(535, 25, 75, 260), 'value': 'queen1'},
+                   {'rect': pygame.Rect(770, 150, 75, 150), 'value': 'rook1'},
+                   {'rect': pygame.Rect(875, 100, 70, 200), 'value': 'bishop1'},
+                   {'rect': pygame.Rect(630, 110, 100, 175), 'value': 'horse1'}]
+        changed = False
+
+        screen.blit(torn_page,(450,-50))
+        for button in buttons:
+            pygame.draw.rect(screen,'yellow',button['rect'])
+
+        for x in buttons:
+            if x['rect'].collidepoint(mouse_pos):
+                if pygame.mouse.get_pressed()[0] == 1:
+                    new_name = selected_piece['name'].replace('pawn',x['value'])
+                    selected_piece.update({'name':new_name})
+                    changed = True
+                    main_game()
+
+        if changed: 
+            turn_manager = 2 if turn_manager == 5 else 1
+
+
+def smooth_movement(start_pos,end_pos,piece,step=50):
+    start_x = start_pos.x
+    start_y = start_pos.y 
+
+    end_x = end_pos.x + 20
+    end_y = end_pos.y + 20
+
+    difference_x = (end_x - start_x) / step
+    difference_y = (end_y - start_y) / step
+
+    for i in range(step + 1):
+        start_x += difference_x
+        start_y += difference_y
+
+        current_x = int(start_x)
+        current_y = int(start_y)
+        
+        piece.update({'rect': pygame.Rect(current_x, current_y, 60, 60)})
+        screen.blit(board,(100,100))
+        pieces_init.draw_pieces()
+        pygame.display.update()
+    
+    piece_move_sfx.play()
+
 def main_game():
-    screen.fill('green')
+    screen.blit(table,(0,0))
     pieces_init.create_pieces()
+    pieces_init.draw_pieces()
     pieces_init.create_possible_moves()
-    board_init.create_board()
 
 turn_manager = 1
 
@@ -193,9 +220,12 @@ clock = pygame.time.Clock()
 square_size = 100
 padding = 20
 font = pygame.font.Font(None, 20)
-
+piece_move_sfx = pygame.mixer.Sound('chess_piece_moved.wav')
 board_init = BoardManager()
 pieces_init = PieceManager(pieces)
+table = pygame.image.load('table.png')
+board = pygame.image.load('chess_board.png')
+torn_page = pygame.image.load('torn_page2.png')
 
 
 main_game()
@@ -208,7 +238,9 @@ while True:
         if event.type == pygame.MOUSEBUTTONDOWN:
             select_piece()
 
-    pieces_init.draw_pieces()
+    if turn_manager != 5 and turn_manager != 6:
+        pieces_init.draw_pieces()
+
 
     pygame.display.update()
     clock.tick(60)
